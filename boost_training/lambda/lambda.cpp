d43d474567677f92aebc68be996082ecad364bc0
Takatoshi Kondo <kondo@t.email.ne.jp>
2010-12-25 15:57:47 +0900

    modify lambda

--- boost_training/lambda/lambda.cpp
+++ boost_training/lambda/lambda.cpp
@@ -20,22 +20,3 @@ int main() {
     std::cout << "\n";
 }
 
-なかなかやるでしょ？
-
-では、int型ではなく、自前のクラスでやってみよう。
-
-rubyではこんな感じ。
-http://ideone.com/xrQKz
-class My 
-    def initialize(val)
-        @val = val
-    end
-    attr_reader :val
-end
- 
-a = [ My.new(5), My.new(3), My.new(1), My.new(2), My.new(4)]
-puts a.inject("") { |result, v| result += v.val.to_s + " " }
-a.sort! { |lhs, rhs| lhs.val <=> rhs.val }
-puts a.inject("") { |result, v| result += v.val.to_s + " " }
-a.sort! { |lhs, rhs| rhs.val <=> lhs.val }
-puts a.inject("") { |result, v| result += v.val.to_s + " " }
