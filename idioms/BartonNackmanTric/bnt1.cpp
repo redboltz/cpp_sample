98be39cea7df209950ab0cd906f58e9d3d211c8d
Takatoshi Kondo <kondo@t.email.ne.jp>
2011-06-04 18:29:52 +0900

    modify inherit class

--- idioms/BartonNackmanTric/bnt1.cpp
+++ idioms/BartonNackmanTric/bnt1.cpp
@@ -7,12 +7,21 @@ public:
 	{
 		std::cout << "Bnt::foo\n";
 	}
+	friend void bar(Derived& d)
+	{
+		d.bar();
+	}
+	friend void haz() {}
 };
 
-class User:private Bnt<User> {
+class User:Bnt<User> { // private inheritance
+public:
+	void bar() { std::cout << "User::bar() called" << std::endl; }
 };
 
 int main() {
 	User u;
 	foo(u);
+	bar(u);
+	baz();
 }
