08fe556d6afd48f129a63a96887dc17e9ea8b8f9
Takatoshi Kondo <kondo@t.email.ne.jp>
2011-07-10 13:15:40 +0900

    path_like parser implement test1:using grammar class test2:direct

--- boost_training/spirit/path_like.cpp
+++ boost_training/spirit/path_like.cpp
@@ -7,37 +7,101 @@
 #include <boost/spirit/include/phoenix.hpp>
 
 struct Result {
+	Result():parentDepth(0) {}
+	int parentDepth;
 	std::vector<std::string> path;
 	std::string elem;
 };
 
 BOOST_FUSION_ADAPT_STRUCT(
     Result,
+	(int, parentDepth)
     (std::vector<std::string>, path)
     (std::string, elem)
 )
 
+namespace qi = boost::spirit::qi;
+namespace phx = boost::phoenix;
 
-int main() {
+template <typename Iterator>
+struct path_like_grammar 
+	: qi::grammar<Iterator, Result()> {
+
+	path_like_grammar():path_like_grammar::base_type(path_like, "path_like") {
+		path_like = parentDepth >> path >> elem;
+		parentDepth = *qi::lit("../")[ qi::_val += 1 ];
+		path = 
+			*(
+				(
+					qi::lexeme[*(qi::char_ - '/')] >> '/'
+				)
+				[phx::push_back(qi::_val, phx::construct<std::string>(phx::begin(qi::_1), phx::end(qi::_1)))]
+			)
+			>> (
+					qi::lexeme[*(qi::char_ - ':')] >> ':'
+				)
+				[phx::push_back(qi::_val, phx::construct<std::string>(phx::begin(qi::_1), phx::end(qi::_1)))];
+		elem = +(qi::char_ );
+#if 1
+		qi::on_error<qi::fail>
+		(
+			path_like
+		  , std::cout
+		        << phx::val("Error! Expecting ")
+				<< qi::_4                               // what failed?
+				<< phx::val(" here: \"")
+				<< phx::construct<std::string>(qi::_3, qi::_2)   // iterators to error-pos, end
+				<< phx::val("\"")
+				<< std::endl
+		);
+#endif
+	}
+	qi::rule<std::string::const_iterator, Result()> path_like;
+	qi::rule<std::string::const_iterator, int()> parentDepth;
+	qi::rule<std::string::const_iterator, std::vector<std::string>()> path;
+	qi::rule<std::string::const_iterator, std::string()> elem;
+
+};
+
+
+namespace {
+
+void test1() {
+	typedef path_like_grammar<std::string::const_iterator> path_like;
 	std::string s = "../../abc/def:ghi";
-	//std::string s = "../../abc";
-	
-	namespace qi = boost::spirit::qi;
-	namespace phx = boost::phoenix;
-
-	int i = 0;
 	Result result;
-	qi::rule<std::string::const_iterator, std::vector<std::string>()> path = 
-		*((qi::lexeme[*(qi::char_ - '/')] >> '/')[phx::push_back(qi::_val, phx::construct<std::string>(phx::begin(qi::_1), phx::end(qi::_1)))])
-       >> (qi::lexeme[*(qi::char_ - ':')] >> ':')[phx::push_back(qi::_val, phx::construct<std::string>(phx::begin(qi::_1), phx::end(qi::_1)))];
-	qi::rule<std::string::const_iterator, std::string()> elem =
-		*(qi::char_ - qi::eol) >> qi::eol;
-
+	path_like pl;
 	bool rval = qi::parse(
 		s.begin(), 
 		s.end(), 
-		(*qi::lit("../")[ phx::ref(i) += 1 ] >> path >> elem
-		),
+		pl,
 		result);
-	std::cout << i << std::endl;
+}
+
+void test2() {
+	std::string s = "../../abc/def:ghi";
+	Result result;
+	qi::rule<std::string::const_iterator, int()> parentDepth
+		= *qi::lit("../")[ qi::_val += 1 ];
+	qi::rule<std::string::const_iterator, std::vector<std::string>()> path
+		=  *((qi::lexeme[*(qi::char_ - '/')] >> '/')
+			[phx::push_back(qi::_val, phx::construct<std::string>(phx::begin(qi::_1), phx::end(qi::_1)))]
+			)
+			>>
+			(qi::lexeme[*(qi::char_ - ':')] >> ':')
+			[phx::push_back(qi::_val, phx::construct<std::string>(phx::begin(qi::_1), phx::end(qi::_1)))];
+	qi::rule<std::string::const_iterator, std::string()> elem = +(qi::char_ );
+	qi::rule<std::string::const_iterator, Result()> path_like = parentDepth >> path >> elem;
+	bool rval = qi::parse(
+		s.begin(), 
+		s.end(), 
+		path_like,
+		result);
+}
+
+}
+
+int main() {
+	test1();
+	test2();
 }
