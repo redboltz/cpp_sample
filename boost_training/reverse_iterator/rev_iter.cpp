98f25d099f7e7d3c6b434e43c9f5edade9eed366
Takatoshi Kondo <kondo@t.email.ne.jp>
2011-09-07 20:21:31 +0900

    add assertion check

--- boost_training/reverse_iterator/rev_iter.cpp
+++ boost_training/reverse_iterator/rev_iter.cpp
@@ -1,79 +1,60 @@
 #include <boost/iterator/reverse_iterator.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/compressed_sparse_row_graph.hpp>
-#include <list>
 #include <iostream>
-
-void test_list() {
-    std::list<int> l;
-    l.push_back(0);
-    l.push_back(1);
-    {
-        std::list<int>::iterator ib = l.begin();
-        std::list<int>::iterator ie = l.end();
-        std::cout << *ib++ << std::endl;
-	    std::cout << *ib++ << std::endl;
-
-    }
-    {
-        std::list<int>::iterator ib = l.begin();
-        std::list<int>::iterator ie = l.end();
-        boost::reverse_iterator<std::list<int>::iterator> rib(ie);
-        boost::reverse_iterator<std::list<int>::iterator> rie(ib);
-	    std::cout << *rib++ << std::endl;
-	    std::cout << *rib++ << std::endl;
-    }
-}
+#include <cassert>
 
 void test_graph() {
     typedef boost::adjacency_list<> graph_type;
-	graph_type g(2);
+    graph_type g(2);
     boost::graph_traits<graph_type>::vertex_iterator ib, ie;
     {
-	    boost::tie(ib, ie) = boost::vertices(g);
-	    std::cout << *ib++ << std::endl; // OK
-	    std::cout << *ib++ << std::endl; // OK
+        boost::tie(ib, ie) = boost::vertices(g);
+        std::cout << *ib++ << std::endl; // OK
+        std::cout << *ib++ << std::endl; // OK
+        assert(ib == ie);
     }
     {
-	    boost::tie(ib, ie) = boost::vertices(g);
+        boost::tie(ib, ie) = boost::vertices(g);
         boost::reverse_iterator<boost::graph_traits<graph_type>::vertex_iterator> rib(ie);
         boost::reverse_iterator<boost::graph_traits<graph_type>::vertex_iterator> rie(ib);
 
-	    std::cout << *rib++ << std::endl; // OK
-	    std::cout << *rib++ << std::endl; // OK
+        std::cout << *rib++ << std::endl; // OK
+        std::cout << *rib++ << std::endl; // OK
+        assert(rib == rie);
     }
 }
 
 void test_csr_graph() {
     typedef boost::compressed_sparse_row_graph<> graph_type;
-	std::pair<int, int> e(0, 1);
+    std::pair<int, int> e(0, 1);
     std::pair<int, int> es[] = { e };
     graph_type g(boost::edges_are_unsorted, &es[0], &es[1], 2);
     boost::graph_traits<graph_type>::vertex_iterator ib, ie;
     {
-	    boost::tie(ib, ie) = boost::vertices(g);
-	    std::cout << *ib++ << std::endl;
-	    std::cout << *ib++ << std::endl;
+        boost::tie(ib, ie) = boost::vertices(g);
+        std::cout << *ib++ << std::endl;  // OK
+        std::cout << *ib++ << std::endl;  // OK
+        assert(ib == ie);
     }
     {
-	    boost::tie(ib, ie) = boost::vertices(g);
+        boost::tie(ib, ie) = boost::vertices(g);
         boost::reverse_iterator<boost::graph_traits<graph_type>::vertex_iterator> rib(ie);
         boost::reverse_iterator<boost::graph_traits<graph_type>::vertex_iterator> rie(ib);
 
-	    std::cout << *rib++ << std::endl; // NG Why?
-	    std::cout << *rib++ << std::endl; // NG Why?
+        std::cout << *rib++ << std::endl; // NG Why?
+        std::cout << *rib++ << std::endl; // NG Why?
+        assert(rib == rie);
     }
     {
-	    boost::tie(ib, ie) = boost::vertices(g);
-	    std::cout << *--ie << std::endl;  // OK
-	    std::cout << *--ie << std::endl;  // OK
+        boost::tie(ib, ie) = boost::vertices(g);
+        std::cout << *--ie << std::endl;  // OK
+        std::cout << *--ie << std::endl;  // OK
+        assert(ib == ie);
     }
 }
 
-
-
 int main() {
-    test_list();
     test_graph();
     test_csr_graph();
 }
