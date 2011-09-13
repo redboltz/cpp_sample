8c66e9a0affb5ce302c05cd969e047fe225b1d61
Takatoshi Kondo <kondo@t.email.ne.jp>
2011-09-13 18:00:11 +0900

    add optional const example

---
+++ boost_training/optional/const_opt.cpp
@@ -0,0 +1,40 @@
+#include <boost/current_function.hpp>
+#include <boost/optional.hpp>
+#include <iostream>
+#include <cassert>
+
+
+struct Foo {
+	void method() {
+		std::cout << BOOST_CURRENT_FUNCTION << std::endl;
+	}
+	void method() const {
+		std::cout << BOOST_CURRENT_FUNCTION << std::endl;
+	}
+};
+
+struct Bar {
+	boost::optional<Foo&> getFoo() {
+		std::cout << BOOST_CURRENT_FUNCTION << std::endl;
+		return foo_;
+	}
+	boost::optional<Foo const&> getFoo() const {
+		std::cout << BOOST_CURRENT_FUNCTION << std::endl;
+		return foo_ ? *foo_ : boost::optional<Foo const&>();
+	}
+	boost::optional<Foo&> foo_;
+};
+
+int main() {
+	boost::optional<Foo&> opref;
+	boost::optional<Foo const&> opcref;
+	Bar b;
+	Foo f;
+	Bar const& br = b;
+	assert(!br.getFoo());
+	assert(!b.getFoo());
+	b.foo_ = f;
+	br.getFoo()->method();
+	b.getFoo()->method();
+}
+
