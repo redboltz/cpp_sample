209ab49e9cd607788ed019b63f13688585ee8625
Takatoshi Kondo <kondo@t.email.ne.jp>
2011-10-23 17:05:28 +0900

    WithDraw -> Withdraw

--- boost_training/msm/atm_example/atm_trade.hpp
+++ boost_training/msm/atm_example/atm_trade.hpp
@@ -11,49 +11,22 @@
 #include "atm_card_detect.hpp"
 
 namespace Atm {
-    namespace msm = boost::msm;
-    namespace msmf = boost::msm::front;
-    namespace mpl = boost::mpl;
-
-    // ----- Events
     struct ChooseWithdraw {};
-
-    // ----- State machine
     template <class AuthMethod>
     struct Trade_:msm::front::state_machine_def<Trade_<AuthMethod> >
     {
-        // States
         struct Choosing:msm::front::state<> 
         {
             template <class Event,class Fsm>
             void on_entry(Event const&, Fsm&) const {
-                std::cout << "[DBG] Enter Choosing" << std::endl;
                 std::cout << "1.Withdraw, 2.N/A, 3.N/A ..." << std::endl;
             }
-            template <class Event,class Fsm>
-            void on_exit(Event const&, Fsm&) const {
-                std::cout << "[DBG] Exit  Choosing" << std::endl;
-            }
         };
-        struct Withdrawing:Withdraw<AuthMethod>
-        {
-            template <class Event,class Fsm>
-            void on_entry(Event const&, Fsm&) const {
-                std::cout << "[DBG] Enter Withdrawing" << std::endl;
-            }
-            template <class Event,class Fsm>
-            void on_exit(Event const&, Fsm&) const {
-                std::cout << "[DBG] Exit  Withdrawing" << std::endl;
-            }
-        };
-
-        // Set initial state
+        struct Withdrawing:Withdraw<AuthMethod> {};
         typedef Choosing initial_state;
-
         typedef typename Withdrawing::template entry_pt<typename Withdraw_<AuthMethod>::Entry>       WithDrawEntry;
         typedef typename Withdrawing::template entry_pt<typename Withdraw_<AuthMethod>::EntryByCard> WithDrawEntryByCard;
         typedef typename Withdrawing::template exit_pt <typename Withdraw_<AuthMethod>::Exit>        WithDrawExit;
-        // Transition table
         struct transition_table:mpl::vector<
             //          Start         Event           Next                 Action      Guard
             msmf::Row < Choosing,     ChooseWithdraw, WithDrawEntry,       msmf::none, msmf::none >,
@@ -61,8 +34,6 @@ namespace Atm {
             msmf::Row < WithDrawExit, msmf::none,     Choosing,            msmf::none, msmf::none >
         > {};
     };
-
-    // Pick a back-end
     template <class AuthMethod>
     struct Trade:msm::back::state_machine<Trade_<AuthMethod> > {};
 }
