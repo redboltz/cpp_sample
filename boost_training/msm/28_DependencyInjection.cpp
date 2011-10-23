b5274af4a858fcd85c443e0b1e855a56ebe17d5f
Takatoshi Kondo <kondo@t.email.ne.jp>
2011-10-23 09:52:49 +0900

    Template Injection. fail

--- boost_training/msm/28_DependencyInjection.cpp
+++ boost_training/msm/28_DependencyInjection.cpp
@@ -4,6 +4,9 @@
 #include <boost/msm/front/state_machine_def.hpp>
 #include <boost/msm/front/functor_row.hpp>
 
+#define USE_TEMPLATE
+
+// Implementation
 namespace {
     namespace msm = boost::msm;
     namespace msmf = boost::msm::front;
@@ -11,20 +14,8 @@ namespace {
     struct StateImp_:msm::front::state_machine_def<StateImp_>
     {
         struct Entry:msm::front::entry_pseudo_state<> {};
-        struct StateImp_1 : msm::front::state<> {
-            // Entry action
-            template <class Event,class Fsm>
-            void on_entry(Event const&, Fsm&) {
-                std::cout << "StateImp_1::on_entry()" << std::endl;
-            }
-            // Exit action
-            template <class Event,class Fsm>
-            void on_exit(Event const&, Fsm&) {
-                std::cout << "StateImp_1::on_exit()" << std::endl;
-            }
-        };
+        struct StateImp_1 : msm::front::state<> {};
         typedef Entry initial_state;
-        // Transition table
         struct transition_table:mpl::vector<
             //          Start  Event       Next          Action      Guard
             msmf::Row < Entry, msmf::none, StateImp_1,   msmf::none, msmf::none >
@@ -33,35 +24,56 @@ namespace {
     typedef msm::back::state_machine<StateImp_> StateImp;
 }
 
+// Interface side
 namespace {
     namespace msm = boost::msm;
     namespace msmf = boost::msm::front;
     namespace mpl = boost::mpl;
 
     struct Event1 {};
-
+#if defined(USE_TEMPLATE)
+    template <class Imp>
+    struct Sm1_:msm::front::state_machine_def<Sm1_<Imp> >
+#else
     struct Sm1_:msm::front::state_machine_def<Sm1_>
+#endif
     {
         struct State1:msm::front::state<> {};
+        // Connect interface to implementation
+#if defined(USE_TEMPLATE)
+        struct State2:Imp {};
+#else
         struct State2:StateImp {};
-
-        // Set initial state
+#endif
         typedef State1 initial_state;
-
         // Transition table
         struct transition_table:mpl::vector<
             //          Start   Event       Next                 Action      Guard
+#if defined(USE_TEMPLATE)
             msmf::Row < State1, Event1,     State2::entry_pt
                                             <StateImp_::Entry>,  msmf::none, msmf::none >
+#else
+            msmf::Row < State1, Event1,     State2::entry_pt
+                                            <StateImp_::Entry>,  msmf::none, msmf::none >
+#endif
         > {};
     };
 
     // Pick a back-end
+#if defined(USE_TEMPLATE)
+    template <class Imp>
+    struct Sm1:msm::back::state_machine<Sm1_<Imp> > {};
+#else
     typedef msm::back::state_machine<Sm1_> Sm1;
+#endif
 
     void test()
     {
+#if defined(USE_TEMPLATE)
+        Sm1<StateImp> sm1;
+#else
         Sm1 sm1;
+#endif
         sm1.start(); 
         std::cout << "> Send Event1" << std::endl;
         sm1.process_event(Event1());
