71aa9ad528fef18a162d538d3d86bcd943d84afa
Takatoshi Kondo <kondo@t.email.ne.jp>
2011-12-13 14:50:15 +0900

    Add copyrights.

--- boost_training/proto/when.cpp
+++ boost_training/proto/when.cpp
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ *           Getting Jiggy with Boost.Proto - Boost'Con 2011 Tutorial
+ *                                 by Joel Falcou
+ *
+ *                             Step 2 : Transformers !
+ *
+ *             Distributed under the Boost Software License, Version 1.0.
+ *                    See accompanying file LICENSE.txt or copy at
+ *                       http://www.boost.org/LICENSE_1_0.txt
+ ******************************************************************************/
+
+
 #include <iostream>
 
 #include <boost/proto/proto.hpp>
