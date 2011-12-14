415986c3196040abea798e303e996949e7b48f8d
Takatoshi Kondo <kondo@t.email.ne.jp>
2011-12-14 17:40:16 +0900

    Add copyright. Tab space convert

--- boost_training/proto/temp_normal.cpp
+++ boost_training/proto/temp_normal.cpp
@@ -1,4 +1,7 @@
 /*******************************************************************************
+ * Modified by Takatoshi Kondo.
+ * This code is based on the below.
+ * -----------------------------------------------------------------------------
  *           Getting Jiggy with Boost.Proto - Boost'Con 2011 Tutorial
  *                                 by Joel Falcou
  *
@@ -24,34 +27,34 @@ struct variable_tag {};
 
 // Grammerの定義
 struct  analytical_function
-	: proto::or_<
-		proto::terminal< variable_tag >, 
-		proto::or_< 
-			proto::terminal< int >, 
-			proto::terminal< float >, 
-			proto::terminal< double >
-		>, 
-		proto::plus<analytical_function, analytical_function>, 
-		proto::negate<analytical_function>, 
-		proto::minus<analytical_function, analytical_function>, 
-		proto::multiplies<analytical_function, analytical_function>, 
-		proto::divides<analytical_function, analytical_function>
-	>
-	{};
+    : proto::or_<
+        proto::terminal< variable_tag >, 
+        proto::or_< 
+            proto::terminal< int >, 
+            proto::terminal< float >, 
+            proto::terminal< double >
+        >, 
+        proto::plus<analytical_function, analytical_function>, 
+        proto::negate<analytical_function>, 
+        proto::minus<analytical_function, analytical_function>, 
+        proto::multiplies<analytical_function, analytical_function>, 
+        proto::divides<analytical_function, analytical_function>
+    >
+    {};
 
 // Grammerのチェック関数
 template<class Expression> inline void
 check_for_match( Expression const& xpr )
 {
-	// 式の表示
-	proto::display_expr(xpr);
-	// 式のチェック
-	std::cout
-		<<  ( proto::matches<Expression, analytical_function>::value
-			? "matches " 
-			: "doesn't match "
-			)
-		<< "the analytical_function grammar\n";
+    // 式の表示
+    proto::display_expr(xpr);
+    // 式のチェック
+    std::cout
+        <<  ( proto::matches<Expression, analytical_function>::value
+            ? "matches " 
+            : "doesn't match "
+            )
+        << "the analytical_function grammar\n";
 }
 
 // Custom operator
@@ -61,42 +64,42 @@ check_for_match( Expression const& xpr )
 
 // 自前で作ってみる
 struct dplus : proto::callable {
-	typedef double result_type;
-	double operator()(double lhs, double rhs) const {
-		std::cout << boost::format("custom plus called!! %1% + %2%\n") % lhs % rhs;
-		return lhs + rhs;
-	}
+    typedef double result_type;
+    double operator()(double lhs, double rhs) const {
+        std::cout << boost::format("custom plus called!! %1% + %2%\n") % lhs % rhs;
+        return lhs + rhs;
+    }
 };
 
 // Evaluator
 struct  evaluate_
-	: proto::or_<
-		// 変数
-		proto::when< 
-			proto::terminal< variable_tag >, 
-			proto::_state
-		>, 
-		// 変数以外のterminal (つまり定数）
-		proto::when< 
-			proto::terminal< proto::_ >, 
-			proto::_value
-		>, 
-		// + 演算子
-		proto::when< 
-			proto::plus < 
-				proto::_,
-				proto::_
-			>,
-			// ここを自前の演算子で実現してみる
-			dplus(
-				evaluate_(proto::_left),
-				evaluate_(proto::_right)
-			)
-		>, 
-		// それ以外はC++のルールに従う
-		proto::otherwise< proto::_default<evaluate_> >
-	>
-	{};
+    : proto::or_<
+        // 変数
+        proto::when< 
+            proto::terminal< variable_tag >, 
+            proto::_state
+        >, 
+        // 変数以外のterminal (つまり定数）
+        proto::when< 
+            proto::terminal< proto::_ >, 
+            proto::_value
+        >, 
+        // + 演算子
+        proto::when< 
+            proto::plus < 
+                proto::_,
+                proto::_
+            >,
+            // ここを自前の演算子で実現してみる
+            dplus(
+                evaluate_(proto::_left),
+                evaluate_(proto::_right)
+            )
+        >, 
+        // それ以外はC++のルールに従う
+        proto::otherwise< proto::_default<evaluate_> >
+    >
+    {};
 
 
 // analytical_domainのための先行宣言
@@ -105,29 +108,29 @@ struct analytical_expression;
 
 // functionとexpressionの関連づけ
 struct analytical_domain
-	: proto::domain< 
-		proto::generator<analytical_expression>, 
-		analytical_function
-	>
+    : proto::domain< 
+        proto::generator<analytical_expression>, 
+        analytical_function
+    >
 {};
 
 // expressionの定義
 template<typename AST>
 struct analytical_expression
-	: proto::extends<AST, analytical_expression<AST>, analytical_domain> {
-	typedef proto::
-	      extends<AST, analytical_expression<AST>, analytical_domain> extendee;
+    : proto::extends<AST, analytical_expression<AST>, analytical_domain> {
+    typedef proto::
+          extends<AST, analytical_expression<AST>, analytical_domain> extendee;
 
-	analytical_expression(AST const& ast = AST()) : extendee(ast) {}
+    analytical_expression(AST const& ast = AST()) : extendee(ast) {}
 
-	BOOST_PROTO_EXTENDS_USING_ASSIGN(analytical_expression)
+    BOOST_PROTO_EXTENDS_USING_ASSIGN(analytical_expression)
 
-	typedef double result_type;
+    typedef double result_type;
 
-	result_type operator()(double v0) const {
-		evaluate_ callee;
-		return callee(*this, v0);
-	}
+    result_type operator()(double v0) const {
+        evaluate_ callee;
+        return callee(*this, v0);
+    }
 };
 
 // placeholderの定義。この方式だと、まだ1つしか定義できない。
@@ -135,6 +138,6 @@ analytical_expression< proto::terminal< variable_tag >::type > const _x;
 
 int main()
 {
-	std::cout << (_x + (2+_x))(5) << "\n";
-	std::cout << (_x * 6)(8) << "\n";
+    std::cout << (_x + (2+_x))(5) << "\n";
+    std::cout << (_x * 6)(8) << "\n";
 }
