100639a85d29b0cd88783b2cfa3940f02056d5fe
Takatoshi Kondo <kondo@t.email.ne.jp>
2011-12-15 08:52:21 +0900

    Add blog code.

---
+++ cpp11/osyo-manga_rvref1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <type_traits>
+#include <memory>
+
+struct My {
+	My() = default;
+	My(My const&) {
+		std::cout << "My copy ctor" << std::endl;
+	}
+	My(My&&) {
+		std::cout << "My move ctor" << std::endl;
+	}
+};
+
+template<typename T>
+struct holder{
+    T value;
+};
+
+template<typename T>
+holder<typename std::remove_reference<T>::type>
+make_holder(T&& t){
+    return { t };
+}
+
+int main() {
+	My m;
+	holder<My> v1 = make_holder(m);     // ok
+	holder<My> v2 = make_holder(My());  // ok but copy
+}
