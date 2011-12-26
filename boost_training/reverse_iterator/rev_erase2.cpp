3b90aa4ba871807d3d03ea70f7a0078e2951aecf
Takatoshi Kondo <kondo@t.email.ne.jp>
2011-12-26 12:12:44 +0900

    add comment and assert.

--- boost_training/reverse_iterator/rev_erase2.cpp
+++ boost_training/reverse_iterator/rev_erase2.cpp
@@ -12,14 +12,24 @@ int main() {
  
     intset_t::reverse_iterator ri = s.rbegin();
     intset_t::reverse_iterator end = s.rend();
+    //       b            e
+    //   re          rb    
+    //     | 1 | 2 | 3 |   
+    //
     while (ri != end) {
         if (*ri == 2) {
+            //       b            e
+            //   re          rb    
+            //           ri base   
+            //     | 1 | 2 | 3 |   
+            //
 #if defined(BOOST_MSVC)
             intset_t::iterator tmpIt = s.erase(--ri.base()); // VC
-			ri = intset_t::reverse_iterator(tmpIt);
-			assert(*ri == 1);
+            ri = intset_t::reverse_iterator(tmpIt);
+            assert(*ri == 1);
 #else  // BOOST_MSVC
             s.erase((++ri).base()); // VC runtime error, g++ OK
+            assert(*ri == 1);
 #endif // BOOST_MSVC
         }
         else {
