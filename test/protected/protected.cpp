56d616f69238537f3aa0c8b8dae63bd1f28fb728
Takatoshi Kondo <kondo@t.email.ne.jp>
2012-03-20 18:44:54 +0900

    added some files

--- test/protected/protected.cpp
+++ test/protected/protected.cpp
@@ -1,48 +1,64 @@
 class Base {
 protected:
-	void foo() {}
+    void foo() {}
 };
 
 class Derived:public Base {
 public:
-	void bar() {
-		// Implicit this
-		foo();
-		// Explicit this
-		this->foo();
-		// this via Derived reference
-		Derived& d = *this;
-		d.foo();
-
-		// this via Base reference
-		Base& b = *this;
-		// b.foo(); // error
-
-		// Derived not this
-		Derived d2;
-		d2.foo();
-
-		// Base not this
-		Base b2;
-		// b2.foo(); //error
-	}
+    void bar();
 };
+
+class MoreDerived:public Derived {
+};
+
+void Derived::bar() {
+    // Implicit this
+    foo();
+    // Explicit this
+    this->foo();
+    // this via Derived reference
+    Derived& d = *this;
+    d.foo();
+
+    // this via Base reference
+    Base& b = *this;
+    // b.foo(); // error
+
+    // Derived not this
+    Derived d2;
+    d2.foo();
+
+    // Base not this
+    Base b2;
+    // b2.foo(); //error
+    
+    MoreDerived md;
+    md.foo();
+}
+
 
 template <typename T>
 class Derived2:public Base {
 public:
-	void bar() {
-		Derived2<T> d;
-		d.foo();
-
-		Derived2<int> di;
-		// di.foo(); // error
+	operator Derived2<char>() {
+		return Derived2<char>();
 	}
+    void bar() {
+        Derived2<T> d;
+        d.foo();
+
+        Derived2<int> di;
+        // di.foo(); // error
+		Derived2<char> dc = di;
+		dc.foo();
+		Derived2<char> dd;
+		dd.foo();
+    }
 };
 
 int main() {
-	Derived d;
-	d.bar();
-	Derived2<char> d2;
-	d2.bar();
+    Derived d;
+    d.bar();
+    Derived2<char> d2;
+    d2.bar();
 }
