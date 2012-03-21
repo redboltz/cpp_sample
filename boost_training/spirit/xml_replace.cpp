6e1119762beabf7fbd36cc45b419aaca9968340c
Takatoshi Kondo <kondo@t.email.ne.jp>
2012-03-21 13:07:54 +0900

    Added xml escape example.

---
+++ boost_training/spirit/xml_replace.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+
+namespace qi = boost::spirit::qi;
+namespace phx = boost::phoenix;
+
+template <typename Iterator>
+struct xml_replacer 
+    : qi::grammar<Iterator, std::string()> {
+    xml_replacer():xml_replacer::base_type(all) {
+        all %= *(+(qi::char_ - '"') 
+            >> qi::char_('"')
+            >> +expr 
+            >> qi::char_('"'));
+        expr
+            = qi::lit('&') >> expr [qi::_val += "&amp;" + qi::_1]
+         // | qi::lit('"') >> expr [qi::_val += "&quot;" + qi::_1]
+            | qi::lit('<') >> expr [qi::_val += "&lt;" + qi::_1]
+            | qi::lit('>') >> expr [qi::_val += "&gt;" + qi::_1]
+            | text [qi::_val += qi::_1];
+        text %= qi::lexeme[+(qi::char_ - qi::char_("&\"<>"))];
+#if 1
+        qi::on_error<qi::fail>
+        (
+            all
+          , std::cout
+                << phx::val("Error! Expecting ")
+                << qi::_4                               // what failed?
+                << phx::val(" here: \"")
+                << phx::construct<std::string>(qi::_3, qi::_2)   // iterators to error-pos, end
+                << phx::val("\"")
+                << std::endl
+        );
+#endif
+    }
+    qi::rule<std::string::const_iterator, std::string()> all;
+    qi::rule<std::string::const_iterator, std::string()> expr;
+    qi::rule<std::string::const_iterator, std::string()> text;
+};
+
+template <typename Iterator>
+struct xml_revert_replacer 
+    : qi::grammar<Iterator, std::string()> {
+    xml_revert_replacer():xml_revert_replacer::base_type(all) {
+        all %= *(+(qi::char_ - '"') 
+            >> qi::char_('"')
+            >> +expr 
+            >> qi::char_('"'));
+        expr
+            = qi::lit("&amp;") >> expr [qi::_val += '&' + qi::_1]
+         // | qi::lit("&quot;") >> expr [qi::_val += '"' + qi::_1]
+            | qi::lit("&lt;") >> expr [qi::_val += '<' + qi::_1]
+            | qi::lit("&gt;") >> expr [qi::_val += '>' + qi::_1]
+            | text [qi::_val += qi::_1];
+        text %= qi::lexeme[+(qi::char_ - '&')];
+#if 1
+        qi::on_error<qi::fail>
+        (
+            all
+          , std::cout
+                << phx::val("Error! Expecting ")
+                << qi::_4                               // what failed?
+                << phx::val(" here: \"")
+                << phx::construct<std::string>(qi::_3, qi::_2)   // iterators to error-pos, end
+                << phx::val("\"")
+                << std::endl
+        );
+#endif
+    }
+    qi::rule<std::string::const_iterator, std::string()> all;
+    qi::rule<std::string::const_iterator, std::string()> expr;
+    qi::rule<std::string::const_iterator, std::string()> text;
+};
+
+
+namespace {
+
+void test1() {
+    xml_replacer<std::string::const_iterator> xr;
+    xml_revert_replacer<std::string::const_iterator> xrr;
+    std::string s = "<Tag attr=\"C1&C2>C3\" />";
+
+    std::string result1;
+    bool rval1 = qi::phrase_parse(
+        s.begin(), 
+        s.end(), 
+        xr,
+        qi::ascii::space,
+        result1);
+
+    std::string result2;
+    bool rval2 = qi::phrase_parse(
+        result1.begin(), 
+        result1.end(), 
+        xrr,
+        qi::ascii::space,
+        result2);
+
+}
+
+}
+
+int main() {
+    test1();
+}
