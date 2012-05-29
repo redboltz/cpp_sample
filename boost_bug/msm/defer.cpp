2b26f9173089abe164e03371e4119daad6aa92a4
Takatoshi Kondo <kondo@t.email.ne.jp>
2012-05-29 18:20:59 +0900

    Added tests.

--- boost_bug/msm/defer.cpp
+++ boost_bug/msm/defer.cpp
@@ -24,43 +24,20 @@ namespace {
             void on_entry(Event const&, Fsm&) const {
                 std::cout << "State2::on_entry()" << std::endl;
             }
-            struct State2_1_:
-#if 1
-                msmf::state_machine_def<State2_1_>
-#else
-                msmf::state<>
-#endif
+            struct State2_1_:msmf::state<>
             {
                 template <class Event,class Fsm>
                 void on_entry(Event const&, Fsm&) const {
                     std::cout << "State2_1::on_entry()" << std::endl;
                 }
-#if 1
-                struct State2_1_1:msmf::state<>
-                {
-                    template <class Event,class Fsm>
-                    void on_entry(Event const&, Fsm&) const {
-                        std::cout << "State2_1_1::on_entry()" << std::endl;
-                    }
-                };
-                struct Entry1:msmf::entry_pseudo_state<0> {};
-                // Actions
-                struct Action2 {
-                    template <class Event, class Fsm, class SourceState, class TargetState>
-                    void operator()(Event const&, Fsm&, SourceState&, TargetState&) {
-                        std::cout << "Action2()" << std::endl;
-                    }
-                };
-                // Set initial state
-                typedef mpl::vector<State2_1_1> initial_state;
-                // Transition table
-                struct transition_table:mpl::vector<
-                    //          Start       Event       Next        Action      Guard
-                    msmf::Row < Entry1,     msmf::none, State2_1_1, msmf::none, msmf::none >,
-                    msmf::Row < State2_1_1, Event1,     msmf::none, Action2,    msmf::none >
-                > {};
-#endif
-            };
+			};
+            struct State2_2_:msmf::state<>
+            {
+                template <class Event,class Fsm>
+                void on_entry(Event const&, Fsm&) const {
+                    std::cout << "State2_2::on_entry()" << std::endl;
+                }
+			};
             struct Entry1:msmf::entry_pseudo_state<0> {};
             // Actions
             struct Action1 {
@@ -70,30 +47,37 @@ namespace {
                     std::cout << "Action1()" << std::endl;
                 }
             };
+            struct Action2 {
+                template <class Event, class Fsm, class SourceState, class TargetState>
+                void operator()(Event const&, Fsm&, SourceState&, TargetState&)
+                {
+                    std::cout << "Action2()" << std::endl;
+                }
+            };
             typedef msm::back::state_machine<State2_1_> State2_1;
             // Set initial state
             typedef mpl::vector<State2_1_> initial_state;
             // Transition table
             struct transition_table:mpl::vector<
-                //          Start      Event       Next                 Action      Guard
-#if 1
-                msmf::Row < Entry1,    msmf::none, State2_1::entry_pt
-                                                   <State2_1_::Entry1>, msmf::none, msmf::none >,
-#else
-                msmf::Row < Entry1,    msmf::none, State2_1_,           msmf::none, msmf::none >,
-#endif
-                msmf::Row < State2_1_, Event1,     msmf::none,          Action1,    msmf::none >
+                //          Start      Event       Next        Action      Guard
+				msmf::Row < Entry1,    msmf::none, State2_1_,  msmf::none, msmf::none >,
+                msmf::Row < State2_1_, Event1,     msmf::none, Action1,    msmf::none >,
+                msmf::Row < State2_1_, msmf::none, State2_2_,  msmf::none, msmf::none >,
+                msmf::Row < State2_2_, Event2,     msmf::none, Action2,    msmf::none >
                 > {};
         };
         typedef msm::back::state_machine<State2_> State2;
 
+        // Enable deferred capability
+        typedef int activate_deferred_events;
         // Set initial state
         typedef State1_ initial_state;
         // Transition table
         struct transition_table:mpl::vector<
-            //          Start    Event   Next              Action      Guard
-            msmf::Row < State1_, msmf::none, State2::entry_pt
-                                         <State2::Entry1>, msmf::none, msmf::none >
+            //          Start    Event   Next              Action       Guard
+            msmf::Row < State1_, Event1, State2::entry_pt
+                                         <State2::Entry1>, msmf::none,  msmf::none >,
+            msmf::Row < State1_, Event2, msmf::none,       msmf::Defer, msmf::none >
         > {};
     };
 
@@ -104,6 +88,8 @@ namespace {
     {        
         Sm1 sm1;
         sm1.start(); 
+        std::cout << "> Send Event2" << std::endl;
+        sm1.process_event(Event2());
         std::cout << "> Send Event1" << std::endl;
         sm1.process_event(Event1());
     }
