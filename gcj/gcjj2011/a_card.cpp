d26d9cf2cfe6f56eda2257a906a0acaf5cf3a281
Takatoshi Kondo <kondo@t.email.ne.jp>
2012-10-12 23:43:32 -0700

    working...

--- gcj/gcjj2011/a_card.cpp
+++ gcj/gcjj2011/a_card.cpp
@@ -1,49 +1,135 @@
+#include <list>
+#include <boost/assert.hpp>
 
-
-struct Block {
-    Block(int begin, int count):begin(begin), count(count) {}
+struct Cont {
+    Cont(int begin, int count):begin_(begin), count_(count) {
+        BOOST_ASSERT(begin > 0);
+        BOOST_ASSERT(count > 0);
+    }
     int get(int n) const { 
-        assert(n <= count);
-        return begin + n;
+        BOOST_ASSERT(n < count_);
+        return begin_ + n;
     }
     int length() const {
-        return count + 1;
+        return count_;
     }
-    Block removeHead(int n) {
-        assert(n < count);
-        return Block(begin + n, count - n);
+    std::list<Cont> remove(int n, int c) {
+        BOOST_ASSERT(c > 0);
+        std::list<Cont> ret;
+        if (n > 0) ret.push_back(Cont(begin_, n));
+        if (n + c < count_) ret.push_back(Cont(get(n + c), count_ - (n + c)));
+        return ret;
     }
-    Block removeTail(int n) {
-        assert(n < count);
-        return Block(begin, count - n);
-    }
-    std::pair<Block, Block> removeCenter(int n, int c) {
-        assert(n + c < count);
-        int backStart = get(n + c + 1);
-        return std::make_pair(Block(begin, n - 1), Block(backStart, count + 1 - backStart));
-    }  
-    int begin;
-    int count;
+private:
+    int begin_;
+    int count_;
 };
+
+void block_test() {
+    {
+        auto b = Cont(1, 1);
+        BOOST_ASSERT(b.length() == 1);
+        BOOST_ASSERT(b.get(0) == 1);
+    }
+    {
+        auto b = Cont(1, 2);
+        BOOST_ASSERT(b.length() == 2);
+        BOOST_ASSERT(b.get(0) == 1);
+        BOOST_ASSERT(b.get(1) == 2);
+        {
+            auto r = b.remove(0, 1);
+            BOOST_ASSERT(r.size() == 1);
+            BOOST_ASSERT(r.front().get(0) == 2);
+            BOOST_ASSERT(r.front().length() == 1);
+        }
+        {
+            auto r = b.remove(1, 1);
+            BOOST_ASSERT(r.size() == 1);
+            BOOST_ASSERT(r.front().get(0) == 1);
+            BOOST_ASSERT(r.front().length() == 1);
+        }
+        {
+            auto r = b.remove(0, 2);
+            BOOST_ASSERT(r.size() == 0);
+        }
+    }
+    {
+        auto b = Cont(1, 3);
+        BOOST_ASSERT(b.length() == 3);
+        BOOST_ASSERT(b.get(0) == 1);
+        BOOST_ASSERT(b.get(1) == 2);
+        BOOST_ASSERT(b.get(2) == 3);
+        {
+            auto r = b.remove(0, 1);
+            BOOST_ASSERT(r.size() == 1);
+            BOOST_ASSERT(r.front().get(0) == 2);
+            BOOST_ASSERT(r.front().length() == 2);
+        }
+        {
+            auto r = b.remove(1, 1);
+            BOOST_ASSERT(r.size() == 2);
+            BOOST_ASSERT(r.front().get(0) == 1);
+            BOOST_ASSERT(r.front().length() == 1);
+            BOOST_ASSERT(r.back().get(0) == 3);
+            BOOST_ASSERT(r.back().length() == 1);
+        }
+        {
+            auto r = b.remove(2, 1);
+            BOOST_ASSERT(r.size() == 1);
+            BOOST_ASSERT(r.front().get(0) == 1);
+            BOOST_ASSERT(r.front().length() == 2);
+        }
+        {
+            auto r = b.remove(0, 2);
+            BOOST_ASSERT(r.size() == 1);
+            BOOST_ASSERT(r.front().get(0) == 3);
+            BOOST_ASSERT(r.front().length() == 1);
+        }
+    }
+}
+
 
 struct Deck {
     Deck(int num) {
-        assert(num > 0);
-        blocks.push_back(Blocks(1, num - 1));
+        BOOST_ASSERT(num > 0);
+        blocks.push_back(Conts(1, num - 1));
     }
     void suffle(int n, int c) {
+        BOOST_ASSERT(n >= 0);
+        BOOST_ASSERT(c >= 0);
+        auto b = blocks.begin();
+        auto e = blocks.cend();
+        while (b != e) {
+            auto len = b->length();
+            if (n < len) {
+                len -= n;
+                do {
+                    auto ret = remove(n, c);
+                    b = blocks.erase(b);
+                    blocks.insert(b, ret.begin(), ret.end());
+                    
+                } while (c < len);
+                return;
+            }
+            n -= len;
+            ++b;
+        }
     }
     int get(int n) const {
-        assert(n > 0);
-        auto b = blocks.begin();
-        auto e = blocks.end();
+        BOOST_ASSERT(n >= 0);
+        auto b = blocks.cbegin();
+        auto e = blocks.cend();
         while (b != e) {
-            if (n <=  b->length()) {
-                return b->get(n - 1);
+            if (n < b->length()) {
+                return b->get(n);
             }
             n -= b->length();
             ++b;
         }
     }
-    std::list<Block> blocks;
+    std::list<Cont> blocks;
 };
+
+int main() {
+    block_test();
+}
