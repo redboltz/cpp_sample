ba99b2b168cddaaa4c0acc659ccc069c5d7ecab0
Takatoshi Kondo <kondo@t.email.ne.jp>
2012-10-19 16:00:32 -0700

    Renamed a class. Added iterator_range example.

--- boost_training/iterator/const.cpp
+++ boost_training/iterator/const.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <vector>
 
-#include <boost/range/algorithm.hpp>
 #include <boost/assign/list_of.hpp>
 #include <boost/foreach.hpp>
 #include <boost/format.hpp>
-#include <boost/type_traits.hpp>
-
-#include <boost/iterator/iterator_facade.hpp>
-
 #include <boost/mpl/print.hpp>
 
+#include <boost/range/algorithm.hpp>
+#include <boost/range/iterator_range.hpp>
+
+#include <boost/type_traits.hpp>
+#include <boost/iterator/iterator_facade.hpp>
+
+
 template <class Iter>
-class MyIter : public boost::iterator_facade<
-                   MyIter<Iter>,
+class const_deref_iterator : public boost::iterator_facade<
+                   const_deref_iterator<Iter>,
                    typename Iter::value_type,
                    typename Iter::iterator_category,
                    typename boost::add_pointer< 
@@ -26,10 +28,10 @@ class MyIter : public boost::iterator_facade<
                >
 {
 public:
-    typedef typename ::MyIter<Iter> this_type;
+    typedef typename ::const_deref_iterator<Iter> this_type;
     typedef typename Iter::iterator_category iterator_category;
-    MyIter<Iter>(Iter const& base):base_(base) {}
-    typename MyIter::reference dereference() const {
+    const_deref_iterator<Iter>(Iter const& base):base_(base) {}
+    typename const_deref_iterator::reference dereference() const {
         return *base_;
     }
     bool equal(this_type const& other) const {
@@ -54,7 +56,7 @@ private:
 template <class T>
 class ConstRange {
 public:
-    typedef MyIter<typename T::const_iterator> const_iterator;
+    typedef const_deref_iterator<typename T::const_iterator> const_iterator;
     typedef typename const_iterator::reference const_reference;
     ConstRange<T>(T const& t):t_(t) {}
     const_iterator begin() const { return t_.begin(); }
@@ -79,6 +81,23 @@ void foo(ConstRange<T> const& t) {
     //t[0] = 1;
 }
 
+template <class T>
+using const_deref_iterator_range = boost::iterator_range<const_deref_iterator<T>>;
+
+template <class T>
+const_deref_iterator_range<typename T::const_iterator> make_const_deref_iterator_range(T const& t) {
+    return const_deref_iterator_range<typename T::const_iterator>(t);
+}
+
+template <class T>
+void bar(const_deref_iterator_range<T> const& t) {
+    std::cout << t[0] << std::endl;
+    std::cout << **t.begin() << std::endl;
+    //int *p = t[0];
+    //int *q = *t.begin();
+    //t[0] = 1;
+}
+
 
 int main() {
     namespace range = boost::range;
@@ -88,8 +107,8 @@ int main() {
         std::vector<int *> v1 = assign::list_of(&v[0])(&v[1])(&v[2])(&v[3])(&v[4]);
         std::vector<int *> v2 = assign::list_of(&v[0])(&v[1])(&v[2])(&v[5]);
 
-        MyIter<std::vector<int*>::iterator> ib(v2.begin());
-        MyIter<std::vector<int*>::iterator> ie(v2.end());
+        const_deref_iterator<std::vector<int*>::iterator> ib(v2.begin());
+        const_deref_iterator<std::vector<int*>::iterator> ie(v2.end());
         std::vector<int *> v4;
         std::set_difference(v1.begin(), v1.end(), ib, ie, std::back_inserter(v4));
         BOOST_FOREACH(int* i, v4) {
@@ -104,6 +123,7 @@ int main() {
 
         ConstRange<std::vector<int*> > v3(v2);
         foo(make_ConstRange(v2));
+        bar(make_const_deref_iterator_range(v2));
         std::vector<int *> v4;
         range::set_difference(v1, v2, std::back_inserter(v4));
         BOOST_FOREACH(int* i, v4) {
